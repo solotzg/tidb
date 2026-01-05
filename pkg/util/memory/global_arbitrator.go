fcbc014117f4d61574664154264e49a9f38e9393
Zhigao TONG <tongzhigao@pingcap.com>
2026-01-05 15:25:31 +0800

    4: add metrics `available`
    Signed-off-by: Zhigao TONG <tongzhigao@pingcap.com>

--- pkg/util/memory/global_arbitrator.go
+++ pkg/util/memory/global_arbitrator.go
@@ -108,8 +108,9 @@ func reportGlobalMemArbitratorMetrics() {
 			metrics.SetGlobalMemArbitratorGauge(metrics.GlobalMemArbitratorQuota, label, value)
 		}
 		setQuota("allocated", m.allocated())
-		setQuota("out-of-control", m.avoidance.size.Load())
+		setQuota("out-of-control", m.OutOfControl())
 		setQuota("buffer", m.reservedBuffer())
+		setQuota("available", m.available())
 		setQuota("tracked-heap", m.avoidance.heapTracked.Load())
 		setQuota("awaitfree-pool-cap", m.awaitFreePoolCap())
 		setQuota("awaitfree-pool-used", m.approxAwaitFreePoolUsed().quota)
